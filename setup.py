a2ab554a238398289fe7c56603815d8564068811
rayliegh7 <suryakantghodake20@gmail.com>
2023-05-04 11:02:16 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages,setup
+from typing import List
+
+def get_requirement(file_name:str)->List[str]:
+    requirement=[]
+    with open (file_name) as file_obj:
+        requirements=file_obj.readlines()
+        requirement=[req.replace("\n"," ") for req in requirements]
+        if '-e .' in requirement:
+            requirement.remove('-e .')
+    return requirement
+
+
+    
+
+setup(
+    name="web scraping with images",
+    author="suryakant ghodke",
+    version="0.0.1",
+    author_email="suryakantghodake20@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirement('requirement.txt') 
+)
